d4543209b5c2234838df6b1e9d8d5f5aaba9cd7f
Jo <41496238+dongjunn@users.noreply.github.com>
2019-07-21 00:53:07 +1000

    Update install_wm.sh

--- install_wm.sh
+++ install_wm.sh
@@ -1,7 +1,26 @@
 : '
 To start bspwm at login add `exec bspwm` to the bottom of your ~/.xinitrc or ~/.xprofile (depending on how your launch your X/Display manager). The example bspwmrc launches sxhkd for you.
 '
-sudo apt-get install libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev
+sudo apt-get update
+
+sudo apt-get install build-essential
+sudo apt-get install libxcb-xinerama0-dev \
+                     libxcb-icccm4-dev \
+                     libxcb-randr0-dev \
+                     libxcb-util0-dev \
+                     libxcb-ewmh-dev \
+                     libxcb-keysyms1-dev \
+                     libxcb-shape0-dev \
+                     libxcb-xtest0-dev \
+                     gcc \
+                     make \
+                     xcb \
+                     git \
+                     libasound2-dev \
+                     libxft-dev \
+                     libx11-xcb-dev \
+                     vim
+                     
 
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
