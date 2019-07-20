d8dc610f71e8e84cc2902d59fc71bd1026cad954
Jo <41496238+dongjunn@users.noreply.github.com>
2019-07-21 00:05:27 +1000

    Add files via upload

---
+++ install_wm.sh
@@ -0,0 +1,17 @@
+: '
+To start bspwm at login add `exec bspwm` to the bottom of your ~/.xinitrc or ~/.xprofile (depending on how your launch your X/Display manager). The example bspwmrc launches sxhkd for you.
+'
+sudo apt-get install libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev
+
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+cd bspwm && make && sudo make install
+cd ../sxhkd && make && sudo make install
+
+# running bspwm
+mkdir -p ~/.config/{bspwm,sxhkd}
+cp /usr/local/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+cp /usr/local/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+chmod u+x ~/.config/bspwm/bspwmrc
+
+
